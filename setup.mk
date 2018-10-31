719d706937e2e7b7c3167af51f94ec75a216893b
Ivan Ukhov <ivan.ukhov@leovegas.com>
2018-10-31 08:16:20 +0100

    Split setup.mk into sections

--- setup.mk
+++ setup.mk
@@ -1,6 +1,8 @@
-image ?= datalab
-machine ?= n1-standard-8
 project ?= $(shell gcloud config get-value project)
-registry ?= eu.gcr.io
 user ?= ${USER}
+
+image ?= datalab
+registry ?= eu.gcr.io
+
+machine ?= n1-standard-8
 zone ?= europe-west4-a
