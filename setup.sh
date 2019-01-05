5a47d72715adf37b674ba0de9d5d25d9348bd768
Ivan Ukhov <ivan.ukhov@gmail.com>
2019-01-05 09:54:57 +0100

    Install for both Python 2 and Python 3

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 apt-get update
 apt-get install -y htop tmux vim
 
+rm -rf /usr/local/envs/py2env/lib/python2.7/site-packages/seaborn*
 rm -rf /usr/local/envs/py3env/lib/python3.5/site-packages/seaborn*
