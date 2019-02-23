a67dcf458c401dd45f4d6bae1dd6b39aeadd0f10
Ivan Ukhov <ivan.ukhov@gmail.com>
2019-02-23 13:10:56 +0100

    Remove dill and update datalab, numpy, scikit-learn, and scipy

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@
 apt-get update
 apt-get install -y htop tmux vim
 
+rm -rf /usr/local/envs/py2env/lib/python2.7/site-packages/dill*
 rm -rf /usr/local/envs/py2env/lib/python2.7/site-packages/seaborn*
+rm -rf /usr/local/envs/py3env/lib/python3.5/site-packages/dill*
 rm -rf /usr/local/envs/py3env/lib/python3.5/site-packages/seaborn*
