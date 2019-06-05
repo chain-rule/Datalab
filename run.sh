459f1b65df55617eb17619d585679472bf39e93c
Ivan Ukhov <ivan.ukhov@gmail.com>
2019-06-05 08:28:17 +0200

    Fix run.sh to activate Python 3

--- run.sh
+++ run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [ ! -d ~/.development ]; then
-  git clone https://github.com/IvanUkhov/.development.git ~/.development && make -C ~/.development
-fi
-
-if [ ! -d ~/.vim ]; then
-  git clone https://github.com/IvanUkhov/.vim.git ~/.vim --recursive && make -C ~/.vim
-fi
-
 if [ ! -d ~/.ssh ]; then
   mkdir ~/.ssh
   ssh-keygen -t ed25519 -N '' -f ~/.ssh/id_ed25519
@@ -17,8 +9,15 @@ rm -rf /root/.ssh
 ln -s ~/.ssh /root/.ssh
 
 if [ ! -f ~/.zshrc ]; then
-  echo 'source activate py3env' >> ~/.zshrc
-  echo 'source ~/.development/zshrc' >> ~/.zshrc
+  echo "emulate bash -c 'source activate py3env'" >> ~/.zshrc
+fi
+
+if [ ! -d ~/.development ]; then
+  git clone https://github.com/IvanUkhov/.development.git ~/.development && make -C ~/.development
+fi
+
+if [ ! -d ~/.vim ]; then
+  git clone https://github.com/IvanUkhov/.vim.git ~/.vim --recursive && make -C ~/.vim
 fi
 
 if [ ! -f ~/.config/matplotlib/matplotlibrc ]; then
